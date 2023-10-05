874acacc89af6c5e82ea747e72362f35a2e3abbf
kirill5k <immotional@aol.com>
2023-10-05 11:19:09 +0100

    Customer entity

--- internal/customer/models.go
+++ internal/customer/models.go
@@ -1,7 +1,16 @@
 package customer
 
+type Entity struct {
+	ID        string `gorm:"primaryKey"`
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	Address   string
+}
+
 type Customer struct {
-	ID        string `gorm:"primaryKey" json:"id"`
+	ID        string `json:"id"`
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
 	Email     string `json:"email"`
