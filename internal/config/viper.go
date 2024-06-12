cfa4d0103633b462348dbf98b183dd38bb32b7ef
kirill5k <immotional@aol.com>
2024-06-12 09:39:53 +0100

    Read environment variables from config

--- internal/config/viper.go
+++ internal/config/viper.go
@@ -1,10 +1,13 @@
 package config
 
 import (
+	"fmt"
 	"github.com/spf13/viper"
 	"kirill5k/go/microservice/internal/database"
 	"kirill5k/go/microservice/internal/server"
 	"log"
+	"os"
+	"strings"
 )
 
 type Config struct {
@@ -21,9 +24,37 @@ func LoadViperConfig() *Config {
 		log.Fatalf("failed to read viper config. %v", err)
 	}
 
+	for _, k := range v.AllKeys() {
+		value := v.GetString(k)
+		if strings.HasPrefix(value, "${") && strings.HasSuffix(value, "}") {
+			envVarName, defaultValue := getEnvVarNameWithDefaultValue(value)
+			v.Set(k, getEnv(envVarName, defaultValue))
+		}
+	}
+
 	var c Config
 	if err := v.Unmarshal(&c); err != nil {
 		log.Fatalf("failed to decode viper config into struct. %v", err)
 	}
 	return &c
+}
+
+func getEnvVarNameWithDefaultValue(stringTemplate string) (string, string) {
+	envVarName := strings.TrimSuffix(strings.TrimPrefix(stringTemplate, "${"), "}")
+	if strings.Contains(envVarName, ":") {
+		split := strings.SplitN(envVarName, ":", 2)
+		return split[0], split[1]
+	}
+	return envVarName, ""
+}
+
+func getEnv(envVarName, defaultValue string) string {
+	value, found := os.LookupEnv(envVarName)
+	if found {
+		return value
+	}
+	if !found && defaultValue != "" {
+		return defaultValue
+	}
+	panic(fmt.Sprintf("Missing required environment variable %s", envVarName))
 }
